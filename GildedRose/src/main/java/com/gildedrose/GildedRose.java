fff3001eb31cdfe4d4515de43b34dc4c0da715a9
rpgueye <rgueye17@gmail.com>
2020-11-11 00:08:02 +0100

    Isolation Sulfura et le reste

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -45,31 +45,22 @@ class GildedRose {
           items[i].quality = 0;
         }
 
+      } else if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+        
+        //DO NOTHING
+
       } else {
         if (items[i].quality > 0) {
-          if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-            //DO NOTHING
-          } else {
-            items[i].quality = items[i].quality - 1;
-          }
+          items[i].quality = items[i].quality - 1;
         }
 
-        if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-          //DO NOTHINGc
-        } else {
-          items[i].sellIn = items[i].sellIn - 1;
-        }
+        items[i].sellIn = items[i].sellIn - 1;
 
         if (items[i].sellIn < 0) {
           if (items[i].quality > 0) {
-            if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-              // TODO
-            } else {
-              items[i].quality = items[i].quality - 1;
-            }
+            items[i].quality = items[i].quality - 1;
           }
         }
-
       }
     } //0
   }
