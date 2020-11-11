fa5baf37f6b4c771a9afee33adf08c61f8c4c086
rpgueye <rgueye17@gmail.com>
2020-11-11 01:05:38 +0100

    Implémentation Switch

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -9,58 +9,62 @@ class GildedRose {
 
   public void updateQuality() {
     for (int i = 0; i < items.length; i++) {
-      if (items[i].name.equals("Aged Brie")) {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1;
-        }
-
-        items[i].sellIn = items[i].sellIn - 1;
-
-        if (items[i].sellIn < 0) {
+      switch (items[i].name) {
+        case "Aged Brie" :
           if (items[i].quality < 50) {
             items[i].quality = items[i].quality + 1;
           }
-        }
 
-      } else if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1;
-        }
+          items[i].sellIn = items[i].sellIn - 1;
 
-        if (items[i].sellIn <= 10) {
+          if (items[i].sellIn < 0) {
+            if (items[i].quality < 50) {
+              items[i].quality = items[i].quality + 1;
+            }
+          }
+          break;
+
+        case "Backstage passes to a TAFKAL80ETC concert" :
           if (items[i].quality < 50) {
             items[i].quality = items[i].quality + 1;
           }
-        }
 
-        if (items[i].sellIn <= 5) {
-          if (items[i].quality < 50) {
-            items[i].quality = items[i].quality + 1;
+          if (items[i].sellIn <= 10) {
+            if (items[i].quality < 50) {
+              items[i].quality = items[i].quality + 1;
+            }
           }
-        }
-        
-        items[i].sellIn = items[i].sellIn - 1;
 
-        if (items[i].sellIn < 0) {
-          items[i].quality = 0;
-        }
+          if (items[i].sellIn <= 5) {
+            if (items[i].quality < 50) {
+              items[i].quality = items[i].quality + 1;
+            }
+          }
 
-      } else if (items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-        
-        //DO NOTHING
+          items[i].sellIn = items[i].sellIn - 1;
 
-      } else {
-        if (items[i].quality > 0) {
-          items[i].quality = items[i].quality - 1;
-        }
+          if (items[i].sellIn < 0) {
+            items[i].quality = 0;
+          }
+          break;
 
-        items[i].sellIn = items[i].sellIn - 1;
+        case "Sulfuras, Hand of Ragnaros" :
 
-        if (items[i].sellIn < 0) {
+          //DO NOTHING
+          break;
+
+        default :
           if (items[i].quality > 0) {
             items[i].quality = items[i].quality - 1;
           }
-        }
+
+          items[i].sellIn = items[i].sellIn - 1;
+
+          if (items[i].sellIn < 0) {
+            if (items[i].quality > 0) {
+              items[i].quality = items[i].quality - 1;
+            }
+          }
       }
     } //0
   }
